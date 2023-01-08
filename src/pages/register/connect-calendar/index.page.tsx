e26adc27158f895a24f92abd45cc40d208604820
Diego-Batista <diegobtistadev@gmail.com>
2023-01-08 10:27:35 -0300

    Configuring NextAuth

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,8 +1,11 @@
 import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { signIn, useSession } from 'next-auth/react'
 import { ArrowRight } from 'phosphor-react'
 import { ConnectBox, ConnectItem, Container, Header } from './styles'
 
 export default function Register() {
+  const session = useSession()
+
   return (
     <Container>
       <Header>
@@ -18,11 +21,17 @@ export default function Register() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant='secondary' size='sm'>
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => signIn('google')}
+          >
             Conectar
             <ArrowRight />
           </Button>
         </ConnectItem>
+
+        <Text>{JSON.stringify(session.data?.user?.email)}</Text>
         <Button type="submit">
           Próximo passo
           <ArrowRight />
