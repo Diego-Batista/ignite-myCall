ba11b07e5d1beb0d4cdafff57afe797459a2bac8
Diego-Batista <diegobtistadev@gmail.com>
2023-01-29 10:45:26 -0300

    Calendar and availability - Visual structure of the timetable

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,45 @@
 import { Calendar } from '../../../../../components/Calendar'
-import { Container } from './styles'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
 
 export function CalendarStep() {
+  const isDateSelected = true
+
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça-feira, <span>20 de setembro</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem disabled>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem disabled>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+            <TimePickerItem>9:00h</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }
