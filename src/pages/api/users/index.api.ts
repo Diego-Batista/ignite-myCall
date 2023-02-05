5b3f967ddde6c0d1848d8fb0ca8158ad2a1424b5
Diego-Batista <diegobtistadev@gmail.com>
2023-02-05 09:56:08 -0300

    Calendar and availability - Finishing query for full days

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,28 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { setCookie } from 'nookies'
-import { prisma } from '../../../lib/prisma'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { setCookie } from "nookies";
+import { prisma } from "../../../lib/prisma";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse,
+  res: NextApiResponse
 ) {
-  if (req.method !== 'POST') {
-    return res.status(405).end()
+  if (req.method !== "POST") {
+    return res.status(405).end();
   }
 
-  const { name, username } = req.body
+  const { name, username } = req.body;
 
   const userExists = await prisma.user.findUnique({
     where: {
       username,
     },
-  })
+  });
 
   if (userExists) {
     return res.status(400).json({
-      message: 'Username já existe.',
-    })
+      message: "Username já existe.",
+    });
   }
 
   const user = await prisma.user.create({
@@ -30,12 +30,12 @@ export default async function handler(
       name,
       username,
     },
-  })
+  });
 
-  setCookie({ res }, '@mycall:userId', user.id, {
+  setCookie({ res }, "@mycall:userId", user.id, {
     maxAge: 60 * 60 * 24 * 7, // 7 days
-    path: '/',
-  })
+    path: "/",
+  });
 
-  return res.status(201).json(user)
+  return res.status(201).json(user);
 }
